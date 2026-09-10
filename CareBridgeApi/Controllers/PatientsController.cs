c78221e40580e50097e71f26d7c1d8a76ddc7e15
anjunthomas <anjunoblethomas@gmail.com>
2026-09-09 23:04:56 -0400

    adding Patient class and start of PatientController

---
+++ CareBridgeApi/Controllers/PatientsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CareBridgeApi.Controllers
+{
+    public class PatientsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CareBridgeApi/Models/Patient.cs
@@ -0,0 +1,16 @@
+﻿namespace CareBridgeApi.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+    }
+}
