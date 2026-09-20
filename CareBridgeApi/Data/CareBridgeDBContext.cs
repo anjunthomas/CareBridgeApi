9a29420ba03734630f3aadd0ab50a1686f5f5598
anjunthomas <anjunoblethomas@gmail.com>
2026-09-20 04:59:46 -0400

    adding data and migrations folders

---
+++ CareBridgeApi/Data/CareBridgeDBContext.cs
@@ -0,0 +1,16 @@
+﻿using CareBridgeApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CareBridgeApi.Data
+{
+    public class CareBridgeDBContext : DbContext
+    {
+        public CareBridgeDBContext(DbContextOptions<CareBridgeDBContext> options)
+            : base(options)
+        {
+        }
+
+        // this is EF Core's representation of the Patients table
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
---
+++ CareBridgeApi/Migrations/20260920081359_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CareBridgeApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    DateOfBirth = table.Column<DateOnly>(type: "date", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Patients");
+        }
+    }
+}
